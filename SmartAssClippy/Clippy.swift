2fc0191361ee9cfbd6dad575344d63e6cd68713b
FreeApp2014 <contact@freeappsw.space>
2019-04-08 12:44:04 +0300

    Initial commit

---
+++ SmartAssClippy/Clippy.swift
@@ -0,0 +1,18 @@
+//
+//  Clippy.swift
+//  SmartAssSoftware
+//
+//  Created by admin on 08/04/19.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var spinningLoader: UIActivityIndicatorView!
+
+    @IBAction func clickedShowHelp(_ sender: AnyObject) {
+    }
+}

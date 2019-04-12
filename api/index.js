23d21cd9bd7b7675ac6e30fbd904060879bad227
FreeApp2014 <contact@freeappsw.space>
2019-04-12 16:05:10 +0300

    A LOT of work including:  - Rewrite everything not to use segues  - Minor redesign  - The generate button now pops you to the question  - Added support for task metadata  - API mockup updated

--- api/index.js
+++ api/index.js
@@ -5,4 +5,84 @@ app.get("/generate", (req, res) => {
     res.send("000");
 })
 
+app.get("/task", (req, res) => {
+    let taskId = req.query.id;
+    //TODO: Perform actual task loading
+    res.status(200);
+    res.send({
+        "info": {
+            "taskId":"000",
+            "name": "Clippy API Demo",
+            "author":"Clippy dev team"
+        },
+        "text": "Hey there tester! Do you like the way this app works?",
+        "qId": "000",
+        "options": [
+            {
+                "text": "YASS!!!11",
+                "rId": "001"
+            },
+            {
+                "text": "No",
+                "rId": "002"
+            },
+            {
+                "text": "Prefer not to say",
+                "rId": "003"
+            },
+            {
+                "text": "Hmmm, is this any special deal?",
+                "rId": "004"
+            }
+        ]
+    })
+})
+app.get("/taskResponse", (req, res) => {
+    //TODO: Do actual stuff
+    let resp;
+    switch (req.query.response){
+        case "001":
+            resp = {
+                "text": "GUD boi",
+                "qId": "000",
+                "options": [
+                    {
+                        "text": "Ok",
+                        "rId": "002"
+                    },
+                    {
+                        "text": "No",
+                        "rId": "001"
+                    }
+                ]
+            };
+            break;
+        case "002":
+            resp = {
+                "text": "bad boi",
+                "qId": "000",
+                "options": [
+                    {
+                        "text": "Ok",
+                        "rId": "001"
+                    },
+                    {
+                        "text": "No",
+                        "rId": "002"
+                    }
+                ]
+            };
+            break;
+        case "003":
+            resp = "";
+            break;
+        case "004":
+            resp = {
+                "otherBS": "Error"
+            }
+    }
+    res.status(200);
+    res.send(resp);
+})
+
 app.listen(9823, "0.0.0.0", () => {console.log("API server started")});
